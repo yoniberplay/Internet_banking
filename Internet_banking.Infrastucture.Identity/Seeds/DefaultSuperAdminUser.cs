e57f891636a72ada1aee9cb0601d9f80620f1112
yencarnacion07 <yoniber.encarnacion@gmail.com>
2022-11-14 20:59:22 -0400

    Estructura realizada, resta factorizacion para adaptar al nuevo proyecto

---
+++ Internet_banking.Infrastucture.Identity/Seeds/DefaultSuperAdminUser.cs
@@ -0,0 +1,40 @@
+﻿using Internet_banking.Core.Application.Enums;
+using Internet_banking.Infrastucture.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_banking.Infrastucture.Identity.Seeds
+{
+    public class DefaultSuperAdminUser
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            ApplicationUser defaultUser = new();
+            defaultUser.FirstName = "root";
+            defaultUser.LastName = "root";
+            defaultUser.UserName = "root";
+            defaultUser.Email = "superadmin@itla.edu.do";
+            defaultUser.EmailConfirmed = true;
+            defaultUser.PhoneNumberConfirmed = true;
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser,"YOniber00+");
+                    await userManager.AddToRoleAsync(defaultUser,Roles.Basic.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.SuperAdmin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Internet_banking.Infrastucture.Identity/ServiceRegistration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Internet_banking.Infrastucture.Identity.Context;
+using Internet_banking.Infrastucture.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Internet_banking.Infrastructure.Identity
+{
+
+    //Extension Method - Decorator
+    public static class ServiceRegistration
+    {
+        public static void AddIdentityInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            #region Contexts
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<IdentityContext>(options => options.UseInMemoryDatabase("IdentityDb"));
+            }
+            else
+            {
+                services.AddDbContext<IdentityContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"),
+                m=> m.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName)));
+            }
+            #endregion
+
+            #region Identity
+            services.AddIdentity<ApplicationUser, IdentityRole>().
+                AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+            #endregion
+        }
+    }
+}
