273f1349fb309bb26fbc55ba902b3429f226f429
yencarnacion07 <yoniber.encarnacion@gmail.com>
2022-11-16 00:15:33 -0400

    program

---
+++ Internet_banking.Infrastucture.Identity/Context/IdentityContext.cs
@@ -0,0 +1,43 @@
+﻿using Internet_banking.Infrastucture.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_banking.Infrastucture.Identity.Context
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) :base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("Identity");
+
+            builder.Entity<ApplicationUser>(entity =>
+            {
+            entity.ToTable("Users");
+                });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable("Roles");
+            });
+
+            builder.Entity<IdentityRole<String>>(entity =>
+            {
+                entity.ToTable("UserRoles");
+            }); 
+
+            builder.Entity<IdentityUserLogin<String>>(entity =>
+            {
+                entity.ToTable("UserLogins");
+            });
+        }
+    }
+}
---
+++ Internet_banking.Infrastucture.Identity/Seeds/DefaultRoles.cs
@@ -0,0 +1,22 @@
+﻿using Internet_banking.Core.Application.Enums;
+using Internet_banking.Infrastucture.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_banking.Infrastucture.Identity.Seeds
+{
+    public class DefaultRoles
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Roles.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Basic.ToString()));
+        }
+
+    }
+}
