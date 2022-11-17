294eae77139574398fbf4ae4936c82b744c69c0f
yencarnacion07 <yoniber.encarnacion@gmail.com>
2022-11-17 07:10:24 -0400

    Punto de partida

--- Internet_banking/Program.cs
+++ Internet_banking/Program.cs
@@ -5,15 +5,16 @@ using Microsoft.AspNetCore.Identity;
 using Internet_banking.Infrastucture.Identity.Entities;
 using Internet_banking.Infrastucture.Identity.Seeds;
 using Internet_banking.Infrastructure.Identity;
+using Internet_banking.Infrastucture.Shared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddSession();
 builder.Services.AddPersistenceInfrastructure(builder.Configuration);
 builder.Services.AddApplicationLayer(builder.Configuration);
+builder.Services.AddShareInfrastructure(builder.Configuration);
 builder.Services.AddIdentityInfrastructure(builder.Configuration);
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddTransient<ValidateUserSession, ValidateUserSession>();
@@ -21,27 +22,26 @@ builder.Services.AddTransient<ValidateUserSession, ValidateUserSession>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-    using (var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    var services = scope.ServiceProvider;
+
+    try
     {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-        var services = scope.ServiceProvider;
+        var usermanager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var rolemanager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
-        try
-        {
-            var usermanager = services.GetRequiredService<UserManager<ApplicationUser>>();
-            var rolemanager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        await DefaultRoles.SeedAsync(usermanager, rolemanager);
+        await DefaultBasicUser.SeedAsync(usermanager, rolemanager);
+        await DefaultSuperAdminUser.SeedAsync(usermanager, rolemanager);
 
-            await DefaultRoles.SeedAsync(usermanager, rolemanager);
-            await DefaultBasicUser.SeedAsync(usermanager, rolemanager);
-            await DefaultSuperAdminUser.SeedAsync(usermanager, rolemanager);
-
-        }
-        catch (Exception ex) { }
     }
+    catch (Exception ex) { }
+}
+   
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -58,4 +58,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=home}/{action=Index}/{id?}");
+
+app.Run();
